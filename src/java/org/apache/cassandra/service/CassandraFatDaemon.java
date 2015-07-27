ed694cb4e3af3d27dfae376a888c68dca314aad8
gdusbabek <gdusbabek@gmail.com>
2015-07-27 15:36:13 -0500

    fat daemon startup

--- src/java/org/apache/cassandra/service/CassandraFatDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraFatDaemon.java
@@ -3,7 +3,13 @@ package org.apache.cassandra.service;
 import org.apache.cassandra.config.Config;
 import org.apache.cassandra.config.DatabaseDescriptor;
 
+import java.net.InetAddress;
+
 public class CassandraFatDaemon {
+    private static final CassandraFatDaemon instance = new CassandraFatDaemon();
+    private CassandraDaemon.Server nativeServer;
+    
+    
     public static void main(String args[]) throws Throwable {
         try {
             DatabaseDescriptor.forceStaticInitialization();
@@ -11,6 +17,12 @@ public class CassandraFatDaemon {
             throw e.getCause();
         }
         Config.setClientMode(true);
+        
         StorageService.instance.initClient();
+        
+        InetAddress nativeAddr = DatabaseDescriptor.getRpcAddress();
+        int nativePort = DatabaseDescriptor.getNativeTransportPort();
+        instance.nativeServer = new org.apache.cassandra.transport.Server(nativeAddr, nativePort);
+        instance.nativeServer.start();
     }
 }
